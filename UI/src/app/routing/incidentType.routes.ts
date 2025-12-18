efbf75c59b1b248518a5ac2b3df89fc0f8d4f6d6
Ruben Freitas <1231267@isep.ipp.pt>
2025-12-18 17:39:38 +0000

    little fix to incident Types routes

--- UI/src/app/routing/incidentType.routes.ts
+++ UI/src/app/routing/incidentType.routes.ts
@@ -4,7 +4,7 @@ export const routes: Routes = [
   {
     path: '',
     loadComponent: () => import('../pages/incident-type/incident-type').then(m => m.IncidentType),
-    data: { $localize: 'Incident Types', roles: ['Admin', 'PortAuthorityOfficer', 'LogisticOperator'] },
+    data: { $localize: 'Incident Types', roles: ['Admin', 'PortAuthorityOfficer'] },
     canActivate: [roleGuard]
   }
 ];
